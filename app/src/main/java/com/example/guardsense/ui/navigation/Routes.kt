d1d43d5480b50e313ed16bad24b368b5b55ff8cb
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-09-22 09:07:09 -0300

    feat: app routes has been added

---
+++ app/src/main/java/com/example/guardsense/ui/navigation/Routes.kt
@@ -0,0 +1,14 @@
+package com.example.guardsense.ui.navigation
+
+object Routes {
+    const val SignInScreen = "signInScreen"
+    const val SignIn01 = "signIn01"
+    // Adicionar todas as rotas do Sign In
+
+    const val Register = "register"
+    const val Dashboard = "dashboard"
+    const val Monitoring = "monitoring"
+    const val DoorManagement = "doorManagement"
+    const val Perfil = "perfil"
+    const val Configurarions = "configurations"
+}
