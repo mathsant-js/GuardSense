1cc997eb94aadb4e5ce1005d3a0e9d55a1dae78e
mathsant-js <matheus.santana114@etec.sp.gov.br>
2025-11-26 12:43:14 -0300

    refactor: provide default values for User model
    Set default values for all properties in the `User` data class. This allows for the instantiation of the `User` object without explicitly providing all arguments.

--- app/src/main/java/com/example/guardsense/data/model/User.kt
+++ app/src/main/java/com/example/guardsense/data/model/User.kt
@@ -3,12 +3,12 @@ package com.example.guardsense.data.model
 import java.util.Date
 
 data class User(
-    val id: String,
-    val name: String,
-    val email: String,
-    val cpf: String,
-    val address: String,
-    val telephone: String,
-    val birthDate: Date,
-    val provider: String,
+    val id: String = "",
+    val name: String = "",
+    val email: String = "",
+    val cpf: String = "",
+    val address: String = "",
+    val telephone: String = "",
+    val birthDate: Date = Date(),
+    val provider: String = ""
 )
